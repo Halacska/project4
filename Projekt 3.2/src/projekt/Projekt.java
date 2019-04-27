ff089028a243c638317e7703ea42dfa3cda1b259
GitGud77 <48094235+GitGud77@users.noreply.github.com>
2019-04-27 22:38:31 +0200

    Add files via upload

---
+++ Projekt 3.2/src/projekt/Projekt.java
@@ -0,0 +1,39 @@
+package projekt;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import projekt.model.Model;
+import projekt.view.FXMLProjektController;
+
+/**
+ *
+ * @author Halacska
+ */
+public class Projekt extends Application {     
+    @Override
+    public void start(Stage primaryStage) throws IOException {        
+        FXMLLoader loader = new FXMLLoader(Projekt.class.getResource("view/FXMLProjekt.fxml"));
+        Scene scene = new Scene(loader.load());
+        String css = this.getClass().getResource("/CSS/TabPane.css").toExternalForm(); ////////////////
+        scene.getStylesheets().add("/CSS/TabPane.css");/////////////////
+        primaryStage.setTitle("Projekt");
+        primaryStage.setScene(scene);
+        ((FXMLProjektController)loader.getController()).setModel(new Model());
+        ((FXMLProjektController)loader.getController()).startController();
+        primaryStage.show();        
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
